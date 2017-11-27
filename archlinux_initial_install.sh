996da4c24a6dea9733da51ec4ddd5ffd42ce4ab4
PINCENT Remi <remi.pincent@inria.fr>
2017-11-27 23:21:08 +0100

    improve post install. Debug initial install

--- archlinux_initial_install.sh
+++ archlinux_initial_install.sh
@@ -11,6 +11,7 @@ DOT_FILES_URL='https://github.com/Lahorde/dotfiles'
 declare -A CONFIG_FILES
 CONFIG_FILES=(['../config_files/netctl/a4h_creativity_lab_wlan0']='/etc/netctl' \
   ['../config_files/samba/smb.conf']='/etc/samba/' \
+  ['../config_files/systemd/timesyncd.conf']='/etc/systemd/timesyncd.conf'
 )
 
 function end
@@ -42,6 +43,7 @@ then
   chroot=0
   if [ "$resp" == 'y' ]
   then
+    root_dir='root' 
     # Do all needed operations before chrooting
     show_main_step 'Prepare chroot environment' 
     show_text 'Copy all config files'
@@ -65,15 +67,26 @@ then
       then
         run_command 'sudo update-binfmts --enable qemu-aarch64 > /dev/null'
         run_command 'sudo cp /usr/bin/qemu-aarch64-static root/usr/bin'
-      elif [[ "$target_arch" =~ ^rpi_armv[6,7]^ ]]
+      elif [[ "$target_arch" =~ ^rpi_armv[6,7]$ ]]
       then 
         run_command 'sudo update-binfmts --enable qemu-arm'
-        run_command 'sudo cp /usr/bin/qemu-arm-static archlinux-rpi/usr/bin'
+        run_command 'sudo cp /usr/bin/qemu-arm-static root/usr/bin'
+      else 
+        show_warning  "raspberry target $target_arch not handled"
+        end
+        exit 1
       fi 
-      run_command 'sudo  arch-chroot root/ /bin/archlinux_initial_install.sh $target_arch' 'Chrooting...'
+    elif [ $target_arch == 'x86_64' ]
+    then 
+      show_text "target is $target_arch"
+      root_dir='root.x86_64' 
     else
       show_warning "target ${target_arch} not handled"
+      end
+      exit 1
     fi
+    run_command 'sudo  arch-chroot root/ /bin/archlinux_initial_install.sh $target_arch' 'Chrooting...'
+    end
     exit 0 
   else
     show_text 'No config file to copy'
@@ -85,55 +98,78 @@ fi
 ##########################################################
 #  All following commands run in newly created system    #
 ##########################################################
-
-run_command 'mount /boot' 'Mounting /boot'         
-
-run_command 'passwd' 'Changing root password' 
-
-run_command  'echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf' 'Changing keymap to FR'
-
-run_command 'ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime' 'Set timezone to Paris' 
-
-run_command 'dircolors --print-database > /etc/DIR_COLORS' 'Get colors for ls'
-
-run_command 'read machine_name' 'Please give a machine name :'
-run_command 'echo $machine_name > /etc/hostname' 'Set machine name'
-run_command 'sed -r -i -e "/127\.0\.0\.1[[:space:]]+localhost\.localdomain[[:space:]]+localhost/s/$/ $machine_name/" /etc/hosts'
-
-
-run_command 'echo LANG=$LANG  > /etc/locale.conf' 'Set LANG'
-run_command 'for loc in "${LOCALES[@]}"; do sed -i -e "s/#$loc/$loc/" /etc/locale.gen ; echo "Set locale $loc"; done;'
+show_main_step 'configuring locales, machine name...'
+if [ ${target_arch:0:3} == 'rpi' ]
+then
+  run_command  ' mount /boot'                                                                                                         ' Mounting /boot'
+fi 
+run_command  ' passwd'                                                                                                              ' Changing root password'
+run_command  ' echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf'                                                                          ' Changing keymap to FR'
+run_command  ' ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime'                                                              ' Set timezone to Paris'
+run_command  ' dircolors --print-database > /etc/DIR_COLORS'                                                                        ' Get colors for ls'
+run_command  ' read machine_name'                                                                                                   ' Please give a machine name :'
+run_command  ' echo $machine_name > /etc/hostname'                                                                                  ' Set machine name'
+run_command  ' sed -r -i -e "/127\.0\.0\.1[[:space:]]+localhost\.localdomain[[:space:]]+localhost/s/$/ $machine_name/" /etc/hosts'
+run_command  ' echo LANG=$LANG  > /etc/locale.conf'                                                                                 ' Set LANG'
+run_command  ' for loc in "${LOCALES[@]}"; do sed -i -e "s/#$loc/$loc/" /etc/locale.gen ; echo "Set locale $loc"; done;'
 run_command 'for loc in "${LOCALES[@]}" ; do echo "$loc" | awk -F "[[:space:]]+" "{file=\"/usr/share/i18n/charmaps/\"\$2\".gz\"; system(\"gunzip --keep \"file)}" ; done;' 'BUG in locale-gen when using QEMU, unzip it manually - refer https://www.reddit.com/r/bashonubuntuonwindows/duplicates/65e36z/psa_localegen_is_bugged_solution_extract_the/' 
 run_command 'locale-gen' 'Generating locale' 
 
+show_main_step 'initialize pacman'
+run_command  ' pacman-key --init'                                                        ' Update pacman key'
+run_command  ' pacman -Syu'                                                              ' Updating packages'
 
-run_command 'pacman-key --init' 'Update pacman key'
-run_command 'pacman -Syu' 'Updating packages'
-run_command 'pacman -S vim python python-pip python2 python2-pip avahi samba tmux wpa_actiond git bluez bluez-utils nss-mdns binutils base-devel distcc alsa-utils xorg-xauth opencv sudo' 'Installing some useful packages...'
+show_main_step 'handle users'
+run_command  'pacman -S sudo'
+run_command  ' userdel -r alarm'                                                         ' Delete user alarm'
+run_command  ' read username'                                                            ' Please give your user name'
+run_command  ' useradd -m -G wheel -s /bin/bash $username'
+run_command  ' passwd $username'
+run_command  ' sed -i -e "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers'  ' allow members of group wheel to execute any command'
+run_command  ' usermod -a -G audio remi'                                                 ' add user $username to group audio'
 
-run_command 'sed -i "s/#X11Forwarding.*$/X11Forwarding yes/" /etc/ssh/sshd_config' 'Enable X11 forwarding over ssh'
-run_command 'sed -i "s/#AllowTcpForwarding.*$/AllowTcpForwarding yes/" /etc/ssh/sshd_config'
-run_command 'sed -i "s/#X11UseLocalhost.*$/X11UseLocalhost yes/" /etc/ssh/sshd_config'
-run_command 'sed -i "s/#X11DisplayOffset.*$/X11DisplayOffset 10/" /etc/ssh/sshd_config'
+show_main_step 'install some useful packages with pacman'
+run_command 'pacman -S vim python python-pip python2 python2-pip avahi samba tmux wpa_actiond git bluez bluez-utils nss-mdns binutils base-devel distcc alsa-utils xorg-xauth opencv wget' 
 
-run_command 'userdel -r alarm' 'Delete user alarm'
+show_main_step 'configuring ssh'
+run_command 'read enable_x11_forward' 'Do you want to enable X11 forwarding? \(y\)es / \(n\)o\)?'
+if [ "$enabl_x11_forward" == 'y' ]
+then 
+  run_command 'sed -i "s/#X11Forwarding.*$/X11Forwarding yes/" /etc/ssh/sshd_config' 'Enable X11 forwarding over ssh'
+  run_command 'sed -i "s/#AllowTcpForwarding.*$/AllowTcpForwarding yes/" /etc/ssh/sshd_config'
+  run_command 'sed -i "s/#X11UseLocalhost.*$/X11UseLocalhost yes/" /etc/ssh/sshd_config'
+  run_command 'sed -i "s/#X11DisplayOffset.*$/X11DisplayOffset 10/" /etc/ssh/sshd_config'
+fi 
 
-run_command 'read username' 'Please give your user name'
-run_command 'useradd -m -G wheel -s /bin/bash $username'
-run_command 'passwd $username'
-run_command 'sed -i -e "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers' 'allow members of group wheel to execute any command'
+show_main_step 'configuring avahi'
+run_command 'sed -i "s/^hosts: files mymachines resolve \[!UNAVAIL=return\] dns myhostname$/hosts: files mdns_minimal \[NOTFOUND=return\] dns/" /etc/nsswitch.conf' 'Configure nsswitch.conf for avahi'
 
-run_command 'sed -e "s/^hosts: files mymachines resolve \[!UNAVAIL=return\] dns myhostname$/hosts: files mdns_minimal \[NOTFOUND=return\] dns/" /etc/nsswitch.conf' 'Configure nsswitch.conf for avahi'
-run_command 'smbpasswd -a $username' 'add user $username to samba users'
+show_main_step 'configuring samba'
+run_command 'read enable_samba' 'Do you want to configure and enable samba shares? \(y\)es / \(n\)o\)?'
+if [ "$enable_samba" == 'y' ]
+then 
+  run_command 'read samba_group' 'please enter a samba group name' 
+  run_command 'groupadd $samba_group'
+  run_command 'usermod -a -G $samba_group $username' 'add $username to group $samba_group'
+  run_command 'smbpasswd -a $username' 'add user $username to samba users'
+  run_command 'systemctl enable smbd' 'enable samba'
+fi 
 
 # RPI specific
-if [ ${target_arch:0:3} == 'rpi']
+if [ "${target_arch:0:3}" == 'rpi' ]
 then
-  show_main_step 'Do Raspberry specific installation'
+  show_main_step 'do Raspberry specific installation'
   run_command 'pacman -S i2c-tools' 'Install Raspberry specific packages'    
   if [[ $target_arch =~ ^rpi_armv[6,7]^ ]]
   then 
     run_command 'pacman -S wiringpi' 'Install Raspberry armv6/v7 specific packages'
+  fi
+  
+  if [[ $target_arch =~ ^rpi_armv[7,8]^ ]]
+  then 
+    run_command 'pacman -S wiringpi' 'Install bluetooth packages for Raspberry 3'
+    run_command 'systemctl enable brcm43438.service' 'enable rpi3 bluetooth service'
+    run_command 'rm /etc/udev/rules.d/50-bluetooth-hci-auto-poweron.rules' 'remove out of date bluetooth rule' 
   fi
   
   run_command 'echo -e "#Enable I2C\ni2c-dev" >> /etc/modules-load.d/raspberrypi.conf'
@@ -143,12 +179,34 @@ then
   run_command 'echo -e "#Enable SPI\ndtparam=spi=on" >> /boot/config.txt' 'Enable SPI'
   run_command 'echo -e "#Enable camera\nstart_x=1\nMin GPU mem for camera\ngpu_mem=128" >> /boot/config.txt' 'Enable Camera'
   run_command 'echo -e "#Enable camera\nbcm2835-v412 >> /etc/modules-load.d/raspberrypi.conf"' 'Add v4l2 driver for camera' 
+else
+  show_main_step 'Do non-Raspberry specific installation'
+  run_command 'pacman -S yaourt' 'Install yaourt'
+  run_command 'yaourt -S jre8'
 fi
 
+run_command 'sed -i -e "s/^.*AutoEnable=.*/AutoEnable=true/" /etc/bluetooth/main.conf' 'enable automatic bluetooth power-on after boot'
 
 show_main_step 'Enable systemd services'
-run_command 'systemctl enable netctl-auto@wlan0.service' 'Enable netctl auto connection using wlan0'
-run_command 'systemctl enable avahi-daemon.service' 'Enable mdns'
-run_command 'systemctl enable smbd'
+run_command 'read wifi_connect' 'Do you want to configure and enable wifi connection using wlan0? \(y\)es / \(n\)o\)?'
+if [ "$enable_x" == 'y' ]
+then
+  run_command  ' systemctl enable netctl-auto@wlan0.service'  ' Enable netctl auto connection using wlan0'
+fi 
+run_command  ' systemctl enable avahi-daemon.service'       ' Enable mdns'
+run_command  ' systemctl enable bluetooth'                  ' enable bluetooth'
 
+show_main_step 'configuring graphic components'
+run_command 'read enable_x' 'Do you want to configure and enable X? \(y\)es / \(n\)o\)?'
+if [ "$enable_x" == 'y' ]
+then
+  run_command 'pacman -R netctl' 'remove netctl'
+  run_command 'pacman -S networkmanager xorg xfce4 plank accountsservice lightdm-gtk-greeter xorg-fonts-type1 ttf-dejavu artwiz-fonts font-bh-ttf  font-bitstream-speedo gsfonts sdl_ttf ttf-bitstream-vera  ttf-cheapskate ttf-liberation  ttf-freefont ttf-arphic-uming ttf-baekmuk network-manager-applet' 'installing graphic related packages'
+fi
+
+show_main_step 'Successful initial install!!!!'
+if [[ $(ls -di) =~ ^2[[:space:]].*$ ]] 
+then 
+  show_warning    'Post install must be done runing archlinux_post_install.sh on target machine'
+fi
 end
--- archlinux_post_install.sh
+++ archlinux_post_install.sh
@@ -6,17 +6,67 @@
 ##################################################################
 
 source "$(dirname "$0")/archlinux_install_common.sh"
+###################################
+#  Add here your configuration    #
+###################################
+DOT_FILES_URL="https://github.com/Lahorde/dotfiles"
+AUR_PACKAGES=('hci-attach-rpi3' 'pi-bluetooth')
+
+function end
+{
+  show_main_step 'end of post install'
+}
+
+function install_aur_package
+{
+  show_text "compile locally $1"
+  run_command "wget -P /tmp/ https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz"
+  run_command "tar -C /tmp/ -zxf /tmp/$1.tar.gz"
+  run_command "pushd /tmp/$1"
+  run_command "makepkg -cs"
+  run_command "sudo pacman -U $1*.tar.gz"
+  run_command "popd /tmp/$1"
+}
 
 INSTALLED_FILES=("$(dirname "$0")/archlinux_install_common.sh" "$(dirname "$0")/archlinux_initial_install.sh" "$0")
 
 show_main_step 'Doing post install - add your dotfiles'
+
+arch=""
+if cat /proc/device-tree/model |grep -i "raspberry pi"
+then
+  show_text 'target is a raspberry'
+  run_command 'res=$(uname -a)' 
+  if [[ "$res" =~ ^.*[[:space:]]armv6l[[:space:]].* ]]
+  then
+    arch='rpi_armv6'
+  elif [[ "$res" =~ ^.*[[:space:]]armv7l[[:space:]].* ]]
+  then
+    arch='rpi_armv7'
+  elif [[ "$res" =~ ^.*[[:space:]]aarch64[[:space:]].* ]]
+  then
+    arch='rpi_armv8'
+  fi
+  show_text "rasperry target architecture is $arch"
+fi
+  
 run_command 'mkdir ~/projects'
-run_command 'git clone https://github.com/Lahorde/dotfiles ~/projects/dotfiles' 'Cloning dot files'
+run_command 'git clone $DOT_FILES_URL ~/projects/dotfiles' 'Cloning dot files'
 run_command '~/projects/dotfiles/install.sh'
+
+if [ -n "$arch" ] && [ ${arch:0:3} == 'rpi' ]
+then
+  show_main_step 'compile locally some packages for raspberry'
+  for package in "${AUR_PACKAGES[@]}" 
+  do 
+    echo $package
+    install_aur_package $package 
+  done
+fi
 
 show_main_step 'Doing other post install steps...'
 run_command 'sudo timedatectl set-ntp true' 'enable ntp synchro'
 
-show_main_steps 'Remove scripts from system'
+show_main_step 'Remove scripts from system'
 run_command 'for script in "${INSTALLED_FILES[@]}" ; do show_text "removing $script"; sudo rm $script; done;'
 
